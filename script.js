f834590108c180f86408c169d1f33dad8aa9fbdb
Glitch (hello-webpage) <none>
2021-04-21 23:09:15 +0000

    🌽📍 Checkpoint ./script.js:26639109/5432

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var member = {
+var addie = {
   id: "nhzfx",
   name: "Addie ☔️ (they/she)",
   color: null,
@@ -26,42 +26,46 @@ var member = {
   suffix: null
 };
 
-/*
-const memberDiv = `
-<div id="${memberNumber.id}" class="row">
+//takes a single member object from PK and returns formatted html as template literal
+
+function buildMemberDiv(member) {
+  return `
+<div id="${member.id}" class="row">
       <img
-        src="${memberNumber.avatar_url}"
-        alt="${memberNumber.name}"
+        src="${member.avatar_url}"
+        alt="${member.name}"
         class="avatar"
       />
 
-      <h2>${memberNumber.display_name === null ? memberNumber.name : memberNumber.display_name}</h2>
+      <h2>${
+        member.display_name === null ? member.name : member.display_name
+      }</h2>
 
       ${
-        memberNumber.pronouns === null
+        member.pronouns === null
           ? ""
           : `<h3>
         Pronouns
       </h3>
       <p>
-        ${memberNumber.pronouns}
+        ${member.pronouns}
       </p>`
       }
      
      ${
-       memberNumber.description === null
+       member.description === null
          ? ""
          : `<h3>
                 Description
       </h3>
       <p>
-        ${memberNumber.description}
+        ${member.description}
       </p>`
      }
       <a href="#contents">Back</a>
     </div>
     `;
-*/
+}
 
 //Builds toc
 function toc(pkOutput) {
@@ -81,18 +85,27 @@ function toc(pkOutput) {
 function renderSite(pkSysID) {
   fetch("https://api.pluralkit.me/v1/s/" + pkSysID + "/members")
     .then(response => response.json())
-    .then(data => {console.log(data)
-                  document.body.innerHTML = JSON.stringify(data)
-                   //map function goes here
-                   //call template function
-                   //join array
-                   //input into members div
-                  })
+    .then(data => {
+      //build TOC - just iterate over each data[].id
+      console.log(data);
+     data.map(function(memberNum){
+       console.log("data array looks like " data[memberNum])
+       buildMemberDiv(data[memberNum])
+     })
+      console.log(members)
+     //map function goes here
+      //call template function
+      //join array
+      //input into members div
+    });
 }
 
+//renderSite("ikgki");
+
+document.body.innerHTML = buildMemberDiv(addie);
 renderSite("ikgki")
 
 //    return `
-  //    ${data.map(function(member){
-    //    `<p>Next up: ${member.id}</p>`
-      //}).join()};`
+//    ${data.map(function(member){
+//    `<p>Next up: ${member.id}</p>`
+//}).join()};`
